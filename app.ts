5b9efe2c3ae87a42ef80c80fb123266618d67bf9
jrking365 <mail@jrking-dev.com>
2023-12-03 12:35:10 -0500

    adding eslint and swagger config

--- app.ts
+++ app.ts
@@ -1,11 +1,24 @@
 import express from 'express';
+import swaggerUi from "swagger-ui-express";
+
 const app = express();
 const port = 3000;
+
+
+app.use(express.static("public"));
+
+app.use(
+    "/docs",
+    swaggerUi.serve,
+    swaggerUi.setup(undefined, {
+      swaggerOptions: {
+        url: "/swagger.json",
+      },
+    })
+);
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
-  return console.log(`Express is listening at http://localhost:${port}`);
-});
+app.listen(port, () => console.log(`Express is listening at http://localhost:${port}`));
